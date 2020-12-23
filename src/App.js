1934a683264ed476205702e893eb3ebca8f12140
maxie <maxiperezc@gmail.com>
2020-12-23 20:37:24 -0300

    fucked

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import * as actionCreators from 'redux/actions'
-//import Main from './layouts/Main'
-import Admin from './layouts/Admin'
+import Main from './layouts/Main'
+// import Admin from './layouts/Admin'
 
 const mapStateToProps = state => {
   return {
@@ -19,8 +19,8 @@ const mapDispatchToProps = dispatch => {
 }
 
 const App = withRouter(
-  //connect(mapStateToProps, mapDispatchToProps)(Main)
-  connect(mapStateToProps, mapDispatchToProps)(Admin)
+  connect(mapStateToProps, mapDispatchToProps)(Main)
+  // connect(mapStateToProps, mapDispatchToProps)(Admin)
 )
 
 export default App
